8cbd7005b68ac29ac8bfaef4147cb0f1fcd61b96
gundaanudeep <gundaanudeep@HDDLBGKIOSK17.Virtusa.com>
2019-06-14 11:43:07 +0530

    first commit

---
+++ src/main/java/test/code/rrr.java
@@ -0,0 +1,15 @@
+package test.code;
+
+public class rrr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+int a,b,c;
+a =10;
+b=20;
+c=a*b;
+System.out.println("This result =" +  c);
+	}
+
+}
